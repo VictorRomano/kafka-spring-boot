19aa8bea1ccb2391a346b3b8338d93275e88caa1
Victor Costa Romano <git@victorromano.com>
2019-07-30 16:56:15 -0300

    Add types KafkaTemplate property

--- src/main/java/com/victorromano/kafkaspringboot/kafka/SampleKafkaProducer.java
+++ src/main/java/com/victorromano/kafkaspringboot/kafka/SampleKafkaProducer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleKafkaProducer {
-    private KafkaTemplate kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
     public SampleKafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
